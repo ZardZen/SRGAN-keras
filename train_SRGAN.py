c0951043cc31f9cb992c2f81fdc14443f62db5fd
Zen <37497157+ZenYugin@users.noreply.github.com>
2019-07-11 16:11:36 +0800

    Update train_SRGAN.py

--- train_SRGAN.py
+++ train_SRGAN.py
@@ -3,12 +3,15 @@ from keras.optimizers import Adam, SGD
 from keras.models import load_model
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, TensorBoard
 import argparse
-from edsr import edsr
+from srgan import srgan, generator, discriminator
 import os
 from keras.utils import multi_gpu_model
 import tensorflow as tf
 from keras import backend as K
 from keras.losses import mean_absolute_error, mean_squared_error
+
+import Utils_model, Utils
+from Utils_model import VGG_LOSS
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
@@ -20,30 +23,14 @@ def psnr(hr, sr):
     hr, sr = _crop_hr_in_training(hr, sr)
     return tf.image.psnr(hr, sr, max_val=255)
 
-def _crop_hr_in_training(hr, sr):
-    """
-    Remove margin of size scale*2 from hr in training phase.
-    The margin is computed from size difference of hr and sr
-    so that no explicit scale parameter is needed. This is only
-    needed for WDSR models.
-
-    """
-    margin = (tf.shape(hr)[1] - tf.shape(sr)[1]) // 2
-    # crop only if margin > 0
-    hr_crop = tf.cond(tf.equal(margin, 0),
-                      lambda: hr,
-                      lambda: hr[:, margin:-margin, margin:-margin, :])
-    hr = K.in_train_phase(hr_crop, hr)
-    hr.uses_learning_phase = True
-    return hr, sr
 def args_parse():
     # construct the argument parse and parse the arguments
     ap = argparse.ArgumentParser(description='Keras Training')
     # ========= paths for training
     ap.add_argument("-npath", "--npy_path", default="data/", required=False,
                     help="path to npy. files to train")
-    ap.add_argument("-mpath", "--model_path", default="model_save/", required=False,
-                    help="path to save the output model")
+#     ap.add_argument("-mpath", "--model_path", default="model_save/", required=False,
+#                     help="path to save the output model")
     ap.add_argument("-lpath", "--log_path", default="log/", required=False,
                     help="path to save the 'log' files")
     ap.add_argument("-name","--model_name", default="edsr.h5", required=False,
@@ -52,12 +39,28 @@ def args_parse():
     ap.add_argument("-p", "--pretrain", default=0, required=False, type=int,
                     help="load pre-train model or not")
 
-    ap.add_argument('-bs', '--batch_size', default=2, type=int,
-                    help='batch size')
-    ap.add_argument('-ep', '--epoch', default=30, type=int,
-                    help='epoch')
+#     ap.add_argument('-bs', '--batch_size', default=2, type=int,
+#                     help='batch size')
+#     ap.add_argument('-ep', '--epoch', default=30, type=int,
+#                     help='epoch')
     ap.add_argument('-m', '--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
+    
+    ap.add_argument('-i', '--input_dir', action='store', dest='input_dir', default='./data/' ,
+                    help='Path for input images')                 
+    ap.add_argument('-o', '--output_dir', action='store', dest='output_dir', default='./output/' ,
+                    help='Path for Output images')
+    ap.add_argument('-mpath', '--model_save_dir', action='store', dest='model_save_dir', default='./model/' ,
+                    help='Path for model')
+    ap.add_argument('-b', '--batch_size', action='store', dest='batch_size', default=8,
+                    help='Batch Size', type=int)                   
+    ap.add_argument('-e', '--epochs', action='store', dest='epochs', default=1000 ,
+                    help='number of iteratios for trainig', type=int)                   
+    ap.add_argument('-n', '--number_of_images', action='store', dest='number_of_images', default=1000 ,
+                    help='Number of Images', type= int)                    
+    ap.add_argument('-r', '--train_test_ratio', action='store', dest='train_test_ratio', default=0.8 ,
+                    help='Ratio of train and test Images', type=float)
+    
     args = vars(ap.parse_args())
     return args
 
@@ -68,13 +71,12 @@ def train(args):
     #X_val = np.load(args["npy_path"] + 'X_val.npy')
     y_train = np.load(args["npy_path"] + 'hr.npy')
     #y_val = np.load(args["npy_path"] + 'y_val.npy')
-    if args["pretrain"]:
-        model = load_model(args["model_path"] + args["model_name"],
-                       custom_objects={'loss': mae, 'psnr': psnr})
-    else:
-        model = edsr(scale, num_filters=256, num_res_blocks=32, res_block_scaling=0.1, tanh_activation=False)
-
-    model.summary()
+#     if args["pretrain"]:
+#         model = load_model(args["model_path"] + args["model_name"],
+#                        custom_objects={'loss': mae, 'psnr': psnr})
+#     else:
+#         model = edsr(scale, num_filters=256, num_res_blocks=32, res_block_scaling=0.1, tanh_activation=False)
+    
     lr_decay = ReduceLROnPlateau(monitor='loss', factor=0.5, patience=10, verbose=1, min_lr=1e-5)
     checkpointer = ModelCheckpoint(args["model_path"] + args["model_name"], verbose=1, save_best_only=True)
     tensorboard = TensorBoard(log_dir=args["log_path"])
@@ -82,18 +84,59 @@ def train(args):
 
     #optimizer = SGD(lr=1e-5, momentum=args["momentum"], nesterov=False)
     optimizer = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999)
-    #parallel_model = multi_gpu_model(model, gpus=1)
-    #parallel_model.compile(loss=mae,metrics=psnr,optimizer=optimizer)
-    model.compile(loss=mae,metrics=[psnr],optimizer=optimizer)
+    
+    G = generator(num_filters=64, num_res_blocks=16)
+    D = discriminator(num_filters=64)
+    G.compile(loss=loss.vgg_loss, optimizer=optimizer)
+    D.compile(loss="binary_crossentropy", optimizer=optimizer)
+    
+    gan = srgan(G, D)
+    gan.compile(loss=[vgg_loss, "binary_crossentropy"],
+                loss_weights=[1., 1e-3],
+                optimizer=optimizer)
+    gan.summary()
+    
+     for e in range(1, epochs+1):
+        print ('-'*15, 'Epoch %d' % e, '-'*15)
+        for _ in tqdm(range(batch_count)):
+       
+            rand_nums = np.random.randint(0, y_train.shape[0], size=batch_size)
+            image_batch_hr = y_train[rand_nums]
+            image_batch_lr = X_train[rand_nums]
+            generated_images_sr = generator.predict(image_batch_lr)
 
-#     EDSR = model.fit(X_train, y_train,validation_data=(X_val, y_val),
-#                                 batch_size=args["batch_size"], epochs=args["epoch"],
-#                                 callbacks=callback_list, verbose=1)
+            real_data_Y = np.ones(batch_size) - np.random.random_sample(batch_size)*0.2
+            fake_data_Y = np.random.random_sample(batch_size)*0.2
+            
+            D.trainable = True
+            
+            d_loss_real = D.train_on_batch(image_batch_hr, real_data_Y)
+            d_loss_fake = D.train_on_batch(generated_images_sr, fake_data_Y)
+            discriminator_loss = 0.5 * np.add(d_loss_fake, d_loss_real)
 
-    #EDSR = parallel_model.fit(X_train, y_train,,validation_split=0.2,batch_size=args["batch_size"], epochs=args["epoch"], callbacks=callback_list, verbose=1)
-    EDSR = model.fit(X_train, y_train,validation_split=0.2,batch_size=args["batch_size"], epochs=args["epoch"], callbacks=callback_list, verbose=1)
+            rand_nums = np.random.randint(0, y_train.shape[0], size=batch_size)
+            image_batch_hr = y_train[rand_nums]
+            image_batch_lr = X_train[rand_nums]
 
+            gan_Y = np.ones(batch_size) - np.random.random_sample(batch_size)*0.2
+            D.trainable = False
+            gan_loss = gan.train_on_batch(image_batch_lr, [image_batch_hr,gan_Y])       
+           
 
+        print("discriminator_loss : %f" % discriminator_loss)
+        print("gan_loss :", gan_loss)
+        gan_loss = str(gan_loss)
+
+        loss_file = open(model_save_dir + 'losses.txt' , 'a')
+        loss_file.write('epoch%d : gan_loss = %s ; discriminator_loss = %f\n' %(e, gan_loss, discriminator_loss) )
+        loss_file.close()
+
+        if e == 1 or e % 5 == 0:
+            Utils.plot_generated_images(output_dir, e, G, x_test_hr, x_test_lr)
+        if e % 5 == 0:
+            G.save(model_save_dir + 'gen_model%d.h5' % e)
+            D.save(model_save_dir + 'dis_model%d.h5' % e)          
+   
 if __name__ == "__main__":
     args = args_parse()
     train(args)
