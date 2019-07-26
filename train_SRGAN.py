e4557a13cc75def1da1079e2c8149f1db20ac8a8
Zen <37497157+ZenYugin@users.noreply.github.com>
2019-07-26 11:50:15 +0800

    Update train_SRGAN.py

--- train_SRGAN.py
+++ train_SRGAN.py
@@ -33,32 +33,6 @@ def get_optimizer():
     adam = Adam(lr=1E-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
     return adam
 
-# def plot_generated_images(output_dir, epoch, generator, x_test_hr, x_test_lr , dim=(1, 3), figsize=(15, 5)):
-
-#     examples = x_test_hr.shape[0]
-#     print(examples)
-#     value = randint(0, examples)
-#     image_batch_hr = denormalize(x_test_hr)
-#     image_batch_lr = x_test_lr
-#     gen_img = generator.predict(image_batch_lr)
-#     generated_image = denormalize(gen_img)
-#     image_batch_lr = denormalize(image_batch_lr)
-
-#     plt.figure(figsize=figsize) 
-#     plt.subplot(dim[0], dim[1], 1)
-#     plt.imshow(image_batch_lr[value], interpolation='nearest')
-#     plt.axis('off')      
-#     plt.subplot(dim[0], dim[1], 2)
-#     plt.imshow(generated_image[value], interpolation='nearest')
-#     plt.axis('off')  
-#     plt.subplot(dim[0], dim[1], 3)
-#     plt.imshow(image_batch_hr[value], interpolation='nearest')
-#     plt.axis('off')   
-#     plt.tight_layout()
-#     plt.savefig(output_dir + 'generated_image_%d.png' % epoch)
-#     plt.close()
-
-    #plt.show()
 def args_parse():
     # construct the argument parse and parse the arguments
     ap = argparse.ArgumentParser(description='Keras Training')
